12ed7d42d84ada6331d5837906721da3f6b056f5
jimbeck <beckerj03@gmail.com>
2017-12-22 21:19:26 -0500

    commented out test

--- src/helpers/RateLimiter.spec.ts
+++ src/helpers/RateLimiter.spec.ts
@@ -1,15 +1,15 @@
-import {InvalidRegionError} from '../errors';
-import {LeagueUrl, UrlBuilder} from './UrlBuilder';
-
-describe('RateLimiter', () => {
-    describe('', () => {
-        it('throws error if invalid region code for the league service', () => {
-            expect(() => UrlBuilder.buildUrl(
-                LeagueUrl.SERVICE,
-                'lol',
-                '123',
-                'blah blah'))
-                .toThrowError(InvalidRegionError);
-        });
-    });
-});
+// import {InvalidRegionError} from '../errors';
+// import {LeagueUrl, UrlBuilder} from './UrlBuilder';
+//
+// describe('RateLimiter', () => {
+//     describe('', () => {
+//         it('throws error if invalid region code for the league service', () => {
+//             expect(() => UrlBuilder.buildUrl(
+//                 LeagueUrl.SERVICE,
+//                 'lol',
+//                 '123',
+//                 'blah blah'))
+//                 .toThrowError(InvalidRegionError);
+//         });
+//     });
+// });
